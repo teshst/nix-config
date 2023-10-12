59d284c96c8ca7ebb46a75517c5b2d9ad6a073b8
teshst <teshpersonal@gmail.com>
2023-10-11 23:59:14 -0600

    added nix doom emacs renamed .doom.d to doom.d

--- users/seth/editors/emacs/default.nix
+++ users/seth/editors/emacs/default.nix
@@ -3,13 +3,9 @@
 {
   imports = [ nix-doom-emacs.hmModule ];
 
-  nixpkgs.overlays = [ emacs-overlay.overlay ];
-
-  home.sessionPath = [ "$XDG_CONFIG_HOME/.emacs.d/bin" ];
-
   programs.doom-emacs = {
-     enable = true;
-     doomPrivateDir = ./.doom.d;
+    enable = true;
+    doomPrivateDir = ./doom.d;
   };
 
   services.emacs.enable = true;
