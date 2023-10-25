fe5438c0c0244bafedce292c98ab1fb4298c2d3d
teshst <teshpersonal@gmail.com>
2023-10-25 00:58:37 -0600

    installed rofi

--- modules/desktop/apps/rofi.nix
+++ modules/desktop/apps/rofi.nix
@@ -11,25 +11,7 @@ in {
   config = mkIf cfg.enable {
 
     user.packages = with pkgs; [
-      (writeScriptBin "rofi" ''
-        #!${stdenv.shell}
-        exec ${pkgs.rofi-wayland}/bin/rofi -terminal kitty -m -1 "$@"
-      '')
-
-      # Fake rofi dmenu entries
-      (makeDesktopItem {
-        name = "rofi-filemenu";
-        desktopName = "Open Directory in Terminal";
-        icon = "folder";
-        exec = "${config.dotfiles.binDir}/rofi/filemenu";
-      })
-
-      (makeDesktopItem {
-        name = "lock-display";
-        desktopName = "Lock screen";
-        icon = "system-lock-screen";
-        exec = "${config.dotfiles.binDir}/zzz";
-      })
+      rofi-wayland
     ];
   };
 }
